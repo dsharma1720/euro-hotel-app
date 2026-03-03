ea683823a15c32b7e008bb76be3481badcf50705
Diksha Sharma <Diksha@Dikshas-Air.local>
2026-03-03 12:56:57 +0530

    Implemented dynamic picklists in Euro Hotel App

--- force-app/main/default/lwc/paymentInfoComponent/paymentInfoComponent.js
+++ force-app/main/default/lwc/paymentInfoComponent/paymentInfoComponent.js
@@ -1,6 +1,12 @@
 import { LightningElement, wire } from 'lwc';
 import { NavigationMixin, CurrentPageReference } from 'lightning/navigation';
 import savePayment from '@salesforce/apex/PaymentController.savePayment';
+import { getObjectInfo, getPicklistValues } from 'lightning/uiObjectInfoApi';
+
+import PAYMENT_OBJECT from '@salesforce/schema/Payment_Master__c';
+import MONTH_FIELD from '@salesforce/schema/Payment_Master__c.Expiry_Month__c';
+import YEAR_FIELD from '@salesforce/schema/Payment_Master__c.Expiry_Year__c';
+
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class PaymentInfoComponent extends NavigationMixin(LightningElement){
@@ -15,36 +21,37 @@ export default class PaymentInfoComponent extends NavigationMixin(LightningEleme
     monthError = '';
     yearError = '';
 
-    monthOptions = [
-        { label: 'January', value: 'January' },
-        { label: 'February', value: 'February' },
-        { label: 'March', value: 'March' },
-        { label: 'April', value: 'April' },
-        { label: 'May', value: 'May' },
-        { label: 'June', value: 'June' },
-        { label: 'July', value: 'July' },
-        { label: 'August', value: 'August' },
-        { label: 'September', value: 'September' },
-        { label: 'October', value: 'October' },
-        { label: 'November', value: 'November' },
-        { label: 'December', value: 'December' }
-    ];
+    monthOptions = [];
+    yearOptions = [];
+       
+    @wire(getObjectInfo, { objectApiName: PAYMENT_OBJECT })
+    objectInfo;
 
-    yearOptions = [
-        { label: '2024', value: '2024' },
-        { label: '2025', value: '2025' },
-        { label: '2026', value: '2026' },
-        { label: '2027', value: '2027' },
-        { label: '2028', value: '2028' },
-        { label: '2029', value: '2029' },
-        { label: '2030', value: '2030' },
-        { label: '2031', value: '2031' },
-        { label: '2032', value: '2032' },
-        { label: '2033', value: '2033' },
-        { label: '2034', value: '2034' },
-        { label: '2035', value: '2035' }
-    ];
+    @wire(getPicklistValues, {
+        recordTypeId: '$objectInfo.data.defaultRecordTypeId',
+        fieldApiName: MONTH_FIELD
+    })
+    monthPicklist({ data, error }) {
+        if (data) {
+            this.monthOptions = data.values;
+        } else if (error) {
+            console.error('Month Picklist Error', error);
+        }
+    }
 
+    @wire(getPicklistValues, {
+        recordTypeId: '$objectInfo.data.defaultRecordTypeId',
+        fieldApiName: YEAR_FIELD
+    })
+    yearPicklist({ data, error }) {
+        if (data) {
+            this.yearOptions = data.values;
+        } else if (error) {
+            console.error('Year Picklist Error', error);
+        }
+    }
+
+  
     @wire(CurrentPageReference)
     getState(pageRef){
         if(pageRef?.state?.c__guestId){
